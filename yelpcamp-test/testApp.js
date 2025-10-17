3c497a1ebdbd42cc3d1ee21e0fe97d249234ddc1
aiden <an010719@naver.com>
2025-10-17 17:25:41 +0900

    edit test

--- yelpcamp-test/testApp.js
+++ yelpcamp-test/testApp.js
@@ -8,11 +8,16 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-console.log(process.env.PORT);
-console.log(process.env.DB_URL);
+console.log("PORT:", port);
+console.log("DB_URL:", process.env.DB_URL);
 
+// Mongoose 연결 테스트
 mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // 연결 제한 시간 10초
+  })
   .then(() => console.log("✅ MongoDB connected!"))
   .catch((err) => console.log("❌ MongoDB connection error:", err));
 
